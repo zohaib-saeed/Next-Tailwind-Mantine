a6d63ab066de1e3881a09ced92ba9686cc7c7644
zohaibzyebi <zyebi553@gmail.com>
2022-12-28 15:26:50 +0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="text-green-400">
+        Wao it is working zohaib
+      </div>
+      <ColorSchemeToggle />
+    </>
+  );
+}
